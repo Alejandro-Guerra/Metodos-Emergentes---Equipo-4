545a773e7b6067e140d92129627d651a6135defc
Chelsea2511 <l21530380@cancun.tecnm.mx>
2026-02-15 21:41:41 -0500

    Mensaje 'No hay hábitos' cuando la lista está vacía #24

---
+++ Proyecto/Mishabitos/Habitos.js
@@ -0,0 +1,8 @@
+
+const btnPrimerHabito = document.getElementById("btnPrimerHabito");
+
+if (btnPrimerHabito) {
+  btnPrimerHabito.addEventListener("click", () => {
+    alert("En el siguiente commit (#25) aquí se abrirá el formulario para crear un hábito ");
+  });
+}
